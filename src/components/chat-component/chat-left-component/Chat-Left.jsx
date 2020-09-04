c001f434c7e916e6fc0be9ea8973d4a42cf8b5b7
vienpham <vienpham268@gmail.com>
2020-09-04 16:22:50 +0700

    hhh

--- src/components/chat-component/chat-left-component/Chat-Left.jsx
+++ src/components/chat-component/chat-left-component/Chat-Left.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./Chat-Left-Styl.css";
 import searchico from "../../../assets/chat/searchico.png";
+import arabic from "../../../assets/avatar/arabic.png";
 
 class ChatLeft extends Component {
   state = {};
@@ -12,19 +13,35 @@ class ChatLeft extends Component {
             <div className="pane-header">
               <h4 class="mb-4">Chats</h4>
               <div className="search mb-3">
-                <div className="search-icon">
-                  <button className="search-button" type="button">
-                    <img src={searchico} alt="" />
-                  </button>
-                </div>
+                <button className="search-button" type="button">
+                  <img src={searchico} alt="" />
+                </button>
                 <input
                   type="text"
-                  className="form-control"
+                  className="input-search"
                   placeholder="Search message or users"
                 />
               </div>
             </div>
-            <div className="pane-body"></div>
+            <div className="pane-body">
+              <h5 className="h5-recent">Recent</h5>
+              <div className="list-recent-chat">
+                <ul className="ul-recent">
+                  <li className="li-recent">
+                    <a href="" className="li-recent-link">
+                      <div className="recent-avatar">
+                        <img src={arabic} alt="" />
+                      </div>
+                      <div className="recent-chat-content">
+                        <h5 className="font-15">Arabic</h5>
+                        <p className='font-14'>hello im here</p>
+                      </div>
+                      <div className="time-left">5 min</div>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+            </div>
           </div>
           <div className="pane ">bbbb</div>
         </div>
